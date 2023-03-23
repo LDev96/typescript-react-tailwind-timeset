645f3502fd19f5c2455476a7936ec5c5ba2b3ba3
LDev96 <128448264+LDev96@users.noreply.github.com>
2023-03-23 18:21:43 +1100

    2 files modified

--- VisitList.tsx
+++ VisitList.tsx
@@ -18,33 +18,38 @@ export default function VisitList({ visits, setVisits }: ownProps) {
       <div className="flex flex-col overflow-y-auto">
         {visits.map((date, index) => {
           const isValidDate = date <= pastDate;
-          return(
-          <div className={`flex mb-1 rounded-full items-center space-x-1 py-1 w-48 ${isValidDate? 'bg-gray-600 ': 'bg-yellow-600' }`}>
-            <XIcon
-              className={'ml-4 h-4 text-white '}
-              onClick={() => {
-                const newVisits = [...visits];
-                newVisits.splice(index, 1);
-                setVisits(newVisits);
-              }}
-            />
-            <DatePicker
-              className="bg-transparent text-white w-40 outline-none"
-              selected={date}
-              onChange={(date) => {
-                const newVisits = [...visits];
-                newVisits.splice(index, 1, date);
-                setVisits(newVisits);
-              }}
-              showTimeSelect
-              showTimeSelectOnly
-              timeIntervals={15}
-              timeCaption="Time"
-              dateFormat="dd/MM/yyyy h:mm aa"
-              disabled={isValidDate}
-            />
-          </div>
-        )})}
+          return (
+            <div
+              className={`flex mb-1 rounded-full items-center space-x-1 py-1 w-48 ${
+                isValidDate ? 'bg-gray-600 ' : 'bg-yellow-600'
+              }`}
+            >
+              <XIcon
+                className={'ml-4 h-4 text-white '}
+                onClick={() => {
+                  const newVisits = [...visits];
+                  newVisits.splice(index, 1);
+                  setVisits(newVisits);
+                }}
+              />
+              <DatePicker
+                className="bg-transparent text-white w-40 outline-none"
+                selected={date}
+                onChange={(date) => {
+                  const newVisits = [...visits];
+                  newVisits.splice(index, 1, date);
+                  setVisits(newVisits);
+                }}
+                showTimeSelect
+                showTimeSelectOnly
+                timeIntervals={15}
+                timeCaption="Time"
+                dateFormat="dd/MM/yyyy h:mm aa"
+                disabled={isValidDate}
+              />
+            </div>
+          );
+        })}
       </div>
     </React.Fragment>
   );
